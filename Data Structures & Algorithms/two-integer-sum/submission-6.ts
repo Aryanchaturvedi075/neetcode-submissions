cb7d3d749d2f05428fba9cffb3cc9a7aa9089906
Aryan Chaturvedi <99679617+Aryanchaturvedi075@users.noreply.github.com>
2026-07-19 00:17:10 +0400

    Add: two-integer-sum - submission-6

---
+++ Data Structures & Algorithms/two-integer-sum/submission-6.ts
@@ -0,0 +1,33 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} target
+     * @return {number[]}
+     */
+    twoSum(nums: number[], target: number): number[] {
+        const map = {};
+        // the issue i was facing in the one-pass approach was a misunderstanding
+        // of adding the diff instead of the nums[i] to the map
+        for (let i = 0; i < nums.length; i++) {
+            const diff = target - nums[i];
+            if (map[diff] !== undefined) return [map[diff], i];
+            map[nums[i]] =  i;
+        }
+
+        return [];
+
+        // the primary logic here is to store the 
+        // difference between the target and each element in a hashmap, 
+        // and store the element's corresponding index for lookup
+        // const map = new Map<number, number>;
+        // for (let i = 0; i < nums.length; i++) map.set(target - nums[i], i);
+
+        // // then reiterate through the loop if a corresponding difference 
+        // // exists as a key in the hashmap, (different index)
+        // for (let i = 0; i < nums.length; i++){
+        //     const diff = target = nums[i], j = map.get(diff);
+        //     if (map.has(diff) && i < j) return [i, j];
+        // }
+        // return [];
+    }
+}

8a15955f0af0cfa12b44669070bc8e56dd35828a
Aryan Chaturvedi <99679617+Aryanchaturvedi075@users.noreply.github.com>
2026-08-01 19:20:02 +0400

    Add: is-anagram - submission-8

---
+++ Data Structures & Algorithms/is-anagram/submission-8.ts
@@ -0,0 +1,17 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @param {string} t
+     * @return {boolean}
+     */
+    isAnagram(s: string, t: string): boolean {
+        const freq: number[] = new Array(26).fill(0);
+        const a: number = 'a'.charCodeAt(0);
+        if (t.length !== s.length) return false;
+        for(let i = 0; i < s.length; i++){
+            freq[s.charCodeAt(i) - a]++;
+            freq[t.charCodeAt(i) - a]--;
+        }
+        return freq.every(x => x === 0);
+    }
+}
